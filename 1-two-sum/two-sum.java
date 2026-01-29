9a5be183f44cb46a1f0e95c85975a15f024412da
Derrick Aboagye <117883472+derrickaboagye@users.noreply.github.com>
2026-01-29 13:36:26 -0500

    Time: 2 ms (99.14%) | Memory: 47.2 MB (48.06%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,17 +1,19 @@
 class Solution {
+    /** we will set first vaule in array as Differnce variable. then we will subtrack target - difference. This will give us a remainder. we will loop through the array to findteh remainder. then we will return {differnce, remainder} */
     public int[] twoSum(int[] nums, int target) {
-        
+        int differnce= nums[0];
+        HashMap <Integer, Integer> map= new HashMap<>();
 
-        HashMap<Integer, Integer> hashy= new HashMap<>();
-
+        //for loop
         for(int i=0;i<nums.length;i++){
-            int diff= target- nums[i];
-            if(hashy.containsKey(diff)){
-                 return new int[]{hashy.get(diff), i};
-
-            }else 
-            hashy.put(nums[i],i);
+            int remain= target- nums[i];
+            
+            if(map.containsKey(remain)){
+                return new int[]{map.get(remain),i};
+            }
+   map.put(nums[i],i);
         }
-        return new int[]{-1,-1};
+              return new int[]{-1, -1}; 
     }
+
 }
